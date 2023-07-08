53595b49417e41f496adf6c0f57ac12a87dbbe28
syobochim <syobochim@gmail.com>
2023-07-08 11:31:15 +0900

    Iteration test and Benchmarking

---
+++ iteration/iteration.go
@@ -0,0 +1,11 @@
+package iteration
+
+const repeatedCount = 5
+
+func Repeat(character string) string {
+	var repeated string
+	for i := 0; i < repeatedCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
