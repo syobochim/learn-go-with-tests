d25725c326d15a77fcd2d882ae5b0f9d5dcee4a7
syobochim <syobochim@gmail.com>
2023-07-14 21:58:48 +0900

    Refactor Using Interface

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,14 +1,26 @@
 package main
 
-type Wallet struct {
-	balance int
+import "fmt"
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	//fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
