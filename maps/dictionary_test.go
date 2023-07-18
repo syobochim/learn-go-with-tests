5cdbd4c9a87b96eca916c3718ee5ba722979f4a1
syobochim <syobochim@gmail.com>
2023-07-18 21:53:00 +0900

    Add feature "Add"

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -17,6 +17,25 @@ func TestSearch(t *testing.T) {
 		assertError(t, got, ErrNotFound)
 	})
 }
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+		dictionary.Add(word, definition)
+
+		assertDefinition(t, dictionary, word, definition)
+	})
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
 
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
@@ -32,4 +51,14 @@ func assertError(t testing.TB, got, want error) {
 	if got != want {
 		t.Errorf("got error %q want %q", got, want)
 	}
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	assertStrings(t, got, definition)
 }
