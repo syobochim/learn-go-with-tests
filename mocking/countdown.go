083519f62b017df4fb1a93e44a5732d28b215ea8
syobochim <syobochim@gmail.com>
2023-07-22 22:23:55 +0900

    Add the first mocking test

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -7,17 +7,29 @@ import (
 	"time"
 )
 
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct {
+}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
