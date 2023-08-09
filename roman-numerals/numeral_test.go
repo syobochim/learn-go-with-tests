eabc604ba4b77391e83e0a543133b3679debcfca
syobochim <syobochim@gmail.com>
2023-08-09 16:54:14 +0900

    Add 4 and 5 cases

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -11,6 +11,8 @@ func TestRomanNumerals(t *testing.T) {
 		{"1 gets convert to I", 1, "I"},
 		{"2 gets convert to II", 2, "II"},
 		{"3 gets convert to III", 3, "III"},
+		{"4 gets convert to IV", 4, "IV"},
+		{"5 gets convert to V", 5, "V"},
 	}
 
 	for _, test := range cases {
