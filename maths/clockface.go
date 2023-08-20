3c945e0bc139838e1fdf90d294ddae0b6351955a
syobochim <syobochim@gmail.com>
2023-08-20 20:24:02 +0900

    Add secondHandPoint test and write enough code

--- maths/clockface.go
+++ maths/clockface.go
@@ -10,6 +10,10 @@ func secondsInRadians(t time.Time) float64 {
 	return math.Pi / (30 / float64(t.Second())) // For floats horrible
 }
 
+func secondHandPoint(t time.Time) Point {
+	return Point{0, -1}
+}
+
 type Point struct {
 	X float64
 	Y float64
