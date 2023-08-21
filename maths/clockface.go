e941c11d288c62a5acc19705fe7658ed065e01ee
syobochim <syobochim@gmail.com>
2023-08-21 21:44:17 +0900

    Add logic at secondHandPoint

--- maths/clockface.go
+++ maths/clockface.go
@@ -5,18 +5,22 @@ import (
 	"time"
 )
 
+type Point struct {
+	X float64
+	Y float64
+}
+
 func secondsInRadians(t time.Time) float64 {
 	// return float64(t.Second()) * math.Pi / 30 // 2π / 60
 	return math.Pi / (30 / float64(t.Second())) // For floats horrible
 }
 
 func secondHandPoint(t time.Time) Point {
-	return Point{0, -1}
-}
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
 
-type Point struct {
-	X float64
-	Y float64
+	return Point{x, y}
 }
 
 func SecondHand(t time.Time) Point {
