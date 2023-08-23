05fb89fb421c1f00544bdcde1004886869f8bcd9
syobochim <syobochim@gmail.com>
2023-08-23 19:21:36 +0900

    Refactor

--- maths/clockface.go
+++ maths/clockface.go
@@ -23,11 +23,15 @@ func secondHandPoint(t time.Time) Point {
 	return Point{x, y}
 }
 
+const secondHandLength = 90
+const clockCentreX = 150
+const clockCentreY = 150
+
 // SecondHand は t.Time でのアナログ時計の秒針の単位ベクトルをPointとして表現したものです。
 func SecondHand(t time.Time) Point {
 	p := secondHandPoint(t)
-	p = Point{p.X * 90, p.Y * 90}   // Scale it to the length of the hand
-	p = Point{p.X, -p.Y}            // Flip it over the X axis to account for the SVG having an origin in the top left hand corner
-	p = Point{p.X + 150, p.Y + 150} // Translate it to the right position (so that it's coming from an origin of (150,150))
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // Scale it to the length of the hand
+	p = Point{p.X, -p.Y}                                      // Flip it over the X axis to account for the SVG having an origin in the top left hand corner
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY}         // Translate it to the right position (so that it's coming from an origin of (150,150))
 	return p
 }
