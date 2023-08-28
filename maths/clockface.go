1f6c6dff07303431728f9fb8636a89b00a1672ae
syobochim <syobochim@gmail.com>
2023-08-28 10:15:29 +0900

    Add minutesInRadians

--- maths/clockface.go
+++ maths/clockface.go
@@ -25,6 +25,10 @@ func secondHandPoint(t time.Time) Point {
 	return Point{x, y}
 }
 
+func minutesInRadians(t time.Time) float64 {
+	return (secondsInRadians(t) / 60) + (math.Pi / (30 / float64(t.Minute())))
+}
+
 const (
 	secondHandLength = 90
 	clockCentreX     = 150
