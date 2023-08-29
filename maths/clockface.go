2759a9a92aca46c1e7234bdbf6d3b9f33d5ab43f
syobochim <syobochim@gmail.com>
2023-08-29 14:23:30 +0900

    Add minuteHandPoint

--- maths/clockface.go
+++ maths/clockface.go
@@ -29,6 +29,13 @@ func minutesInRadians(t time.Time) float64 {
 	return (secondsInRadians(t) / 60) + (math.Pi / (30 / float64(t.Minute())))
 }
 
+func minuteHandPoint(t time.Time) Point {
+	angle := minutesInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+	return Point{x, y}
+}
+
 const (
 	secondHandLength = 90
 	clockCentreX     = 150
