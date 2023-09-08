4650773a0e4c7ba955b1ad0c20aa6a529af5167a
syobochim <syobochim@gmail.com>
2023-09-08 11:18:58 +0900

    Add read directory

--- reading-files/blogposts/blogposts.go
+++ reading-files/blogposts/blogposts.go
@@ -1,10 +1,18 @@
 package blogposts
 
-import "testing/fstest"
+import (
+	"io/fs"
+	"testing/fstest"
+)
 
 type Post struct {
 }
 
 func NewPostsFromFS(fileSystem fstest.MapFS) []Post {
-	return []Post{{}, {}}
+	dir, _ := fs.ReadDir(fileSystem, ".")
+	var posts []Post
+	for range dir {
+		posts = append(posts, Post{})
+	}
+	return posts
 }
