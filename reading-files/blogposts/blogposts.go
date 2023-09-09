a023ffdb90b4519efaf7222264520b8bd53bb426
syobochim <syobochim@gmail.com>
2023-09-09 13:48:43 +0900

    Refactor (using standard library fs.FS)

--- reading-files/blogposts/blogposts.go
+++ reading-files/blogposts/blogposts.go
@@ -2,13 +2,12 @@ package blogposts
 
 import (
 	"io/fs"
-	"testing/fstest"
 )
 
 type Post struct {
 }
 
-func NewPostsFromFS(fileSystem fstest.MapFS) []Post {
+func NewPostsFromFS(fileSystem fs.FS) []Post {
 	dir, _ := fs.ReadDir(fileSystem, ".")
 	var posts []Post
 	for range dir {
