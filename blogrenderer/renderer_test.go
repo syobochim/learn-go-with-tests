32914c685cab5eeef3167e3a94c5da44514b0999
syobochim <syobochim@gmail.com>
2023-09-24 23:03:47 +0900

    Write enough code to make it pass

---
+++ blogrenderer/renderer_test.go
@@ -0,0 +1,33 @@
+package blogrenderer_test
+
+import (
+	"bytes"
+	"learn-go/blogrenderer"
+	"testing"
+)
+
+func TestRender(t *testing.T) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	t.Run("it converts a single post into HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		err := blogrenderer.Render(&buf, aPost)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := buf.String()
+		want := `<h1>hello world</h1>`
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	})
+}
