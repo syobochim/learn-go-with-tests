40188c479fb8c576ed4fb5601e4a75d1ba9e0fd5
syobochim <syobochim@gmail.com>
2023-10-14 12:44:53 +0900

    Using new unified type Stack and interface{}, because StackOfStrings and StackOfInts are almost identical

--- generics/stock.go
+++ generics/stock.go
@@ -1,42 +1,23 @@
 package generics
 
-type StackOfInts struct {
-	values []int
+type StackOfInts = Stack
+type StackOfStrings = Stack
+
+type Stack struct {
+	values []interface{}
 }
 
-func (s *StackOfInts) Push(value int) {
+func (s *Stack) Push(value interface{}) {
 	s.values = append(s.values, value)
 }
 
-func (s *StackOfInts) IsEmpty() bool {
+func (s *Stack) IsEmpty() bool {
 	return len(s.values) == 0
 }
 
-func (s *StackOfInts) Pop() (int, bool) {
+func (s *StackOfInts) Pop() (interface{}, bool) {
 	if s.IsEmpty() {
 		return 0, false
-	}
-	index := len(s.values) - 1
-	el := s.values[index]
-	s.values = s.values[:index]
-	return el, true
-}
-
-type StackOfStrings struct {
-	values []string
-}
-
-func (s *StackOfStrings) Push(value string) {
-	s.values = append(s.values, value)
-}
-
-func (s *StackOfStrings) IsEmpty() bool {
-	return len(s.values) == 0
-}
-
-func (s *StackOfStrings) Pop() (string, bool) {
-	if s.IsEmpty() {
-		return "", false
 	}
 	index := len(s.values) - 1
 	el := s.values[index]
